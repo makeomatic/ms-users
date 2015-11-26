b1f419ecd9f91e0482342f66cfdf55caf50f6195
AVVS <v@aminev.me>
2015-11-26 16:22:03 +0300

    Release v0.5.0
    Implement `$incr` operation in updateMetadata action.
    Semantics are the following:
    
    updateMetadata:
    
    {
      "username",
      "audience",
      "metadata": {
        "$incr": {
          "fieldName": 10,
          "fieldName2": -5
        }
      }
    }
    
    Returns response as in
    
    {
      $incr: {
        fieldName: 10, // did not exist
        fieldName2: -11 // was -6
      }
    }

--- src/actions/updateMetadata.js
+++ src/actions/updateMetadata.js
@@ -14,14 +14,50 @@ module.exports = function updateMetadata(opts) {
   const pipeline = redis.pipeline();
 
   const $remove = metadata.$remove;
-  if ($remove && $remove.length > 0) {
+  const $removeOps = $remove && $remove.length || 0;
+  if ($removeOps > 0) {
     pipeline.hdel(metadataKey, $remove);
   }
 
   const $set = metadata.$set;
-  if ($set && Object.keys($set).length > 0) {
+  const $setKeys = $set && Object.keys($set);
+  const $setLength = $setKeys && $setKeys.length || 0;
+  if ($setLength > 0) {
     pipeline.hmset(metadataKey, ld.mapValues($set, JSON.stringify, JSON));
   }
 
-  return pipeline.exec();
+  const $incr = metadata.$incr;
+  const $incrFields = $incr && Object.keys($incr);
+  const $incrLength = $incrFields && $incrFields.length || 0;
+  if ($incrLength > 0) {
+    $incrFields.forEach(fieldName => {
+      pipeline.hincrby(metadataKey, fieldName, $incr[fieldName]);
+    });
+  }
+
+  return pipeline
+    .exec()
+    .then(responses => {
+      const output = {};
+      let cursor = 0;
+
+      if ($removeOps > 0) {
+        output.$remove = responses[cursor][1];
+        cursor++;
+      }
+
+      if ($setLength > 0) {
+        output.$set = responses[cursor][1];
+        cursor++;
+      }
+
+      if ($incrLength > 0) {
+        const $incrResponse = output.$incr = {};
+        $incrFields.forEach((fieldName, idx) => {
+          $incrResponse[fieldName] = responses[cursor + idx][1];
+        });
+      }
+
+      return output;
+    });
 };

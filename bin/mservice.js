d204e89c0f7ad31b4ea5614a71fac9e69e0ddc02
AVVS <v@aminev.me>
2015-12-25 18:02:52 +0300

    init fake accounts only on NODE_ENV=development

--- bin/mservice.js
+++ bin/mservice.js
@@ -19,12 +19,14 @@ var service = new Service(configuration);
 service.connect()
   .then(function serviceUp() {
     service.log.info('Started service, initiating admin accounts');
-    if (process.env.NODE_ENV === 'production') {
-      return service.initAdminAccounts();
-    } else {
-      return service.initAdminAccounts().then(service.initFakeAccounts);
+    return service.initAdminAccounts();
+  })
+  .tap(function initFakeAccounts() {
+    if (process.env.NODE_ENV !== 'development') {
+      return null;
     }
 
+    return service.initFakeAccounts();
   })
   .catch(function serviceCrashed(err) {
     service.log.fatal('Failed to start service', err);

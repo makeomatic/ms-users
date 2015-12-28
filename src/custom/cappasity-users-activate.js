3d0972cc6f42359bc3a94254c808a1a6e34517a9
AVVS <v@aminev.me>
2015-12-29 01:26:10 +0300

    Add sleep in custom action

--- src/custom/cappasity-users-activate.js
+++ src/custom/cappasity-users-activate.js
@@ -1,3 +1,4 @@
+const Promise = require('bluebird');
 const ld = require('lodash');
 const moment = require('moment');
 const setMetadata = require('../utils/updateMetadata.js');
@@ -13,25 +14,30 @@ module.exports = function mixPlan(username, audience) {
   const route = [payments.prefix, payments.routes.planGet].join('.');
   const id = 'free';
 
-  return amqp
-    .publishAndWait(route, id, { timeout: 5000 })
-    .bind(this)
-    .then(function mix(plan) {
-      const subscription = ld.findWhere(plan.subs, { name: id });
-      const nextCycle = moment().add(1, 'month').format();
-      const update = {
-        username,
-        audience,
-        metadata: {
-          '$set': {
-            plan: id,
-            nextCycle,
-            models: subscription.models,
-            modelPrice: subscription.price,
+  // make sure that ms-payments is launched before
+  return Promise
+    .delay(10000)
+    .then(() => {
+      return amqp
+      .publishAndWait(route, id, { timeout: 5000 })
+      .bind(this)
+      .then(function mix(plan) {
+        const subscription = ld.findWhere(plan.subs, { name: id });
+        const nextCycle = moment().add(1, 'month').format();
+        const update = {
+          username,
+          audience,
+          metadata: {
+            '$set': {
+              plan: id,
+              nextCycle,
+              models: subscription.models,
+              modelPrice: subscription.price,
+            },
           },
-        },
-      };
+        };
 
-      return setMetadata.call(this, update);
+        return setMetadata.call(this, update);
+      });
     });
 };

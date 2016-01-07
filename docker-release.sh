7192d0f679ecfed3d63cbfdf83b0a97207f7654d
AVVS <v@aminev.me>
2016-01-07 18:16:06 +0300

    fix(docker-release) correct default npm-proxy

--- docker-release.sh
+++ docker-release.sh
@@ -2,7 +2,7 @@
 
 docker login -e $DOCKER_EMAIL -p $DOCKER_PWD -u $DOCKER_LOGIN || exit 1
 BUILD_ENV=${ENVS:-production development}
-NPM_PROXY=${NPM_PROXY:-https://registry.npm.com}
+NPM_PROXY=${NPM_PROXY:-https://registry.npmjs.com}
 
 make ENVS="$BUILD_ENV" NPM_PROXY=$NPM_PROXY build
 make ENVS="$BUILD_ENV" NPM_PROXY=$NPM_PROXY push

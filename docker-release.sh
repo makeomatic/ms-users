4ccb3a35b2323bc2e08d9a0dc6c68df1aad2f686
AVVS <v@aminev.me>
2016-01-07 18:03:24 +0300

    fix(docker-release): chmod +x, DOCKER_LOGIN env var

--- docker-release.sh
+++ docker-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker login -e $DOCKER_EMAIL -p $DOCKER_PWD -u $DOCKER_USER || exit 1
+docker login -e $DOCKER_EMAIL -p $DOCKER_PWD -u $DOCKER_LOGIN || exit 1
 BUILD_ENV=${ENVS:-production development}
 
 make ENVS="$BUILD_ENV" build

27db32fe44846f9caef28fa8bbaa59f78f2e0586
Vitaly Aminev <v@aminev.me>
2016-02-22 16:19:28 +0300

    feat(getMetadata): accept `public` opt
    When `public` is set to `true`, then `username` that was passed
    to the function must be an `alias`, otherwise error 404 is thrown

--- src/actions/getMetadata.js
+++ src/actions/getMetadata.js
@@ -1,13 +1,28 @@
 const Promise = require('bluebird');
+const Errors = require('common-errors');
 const getMetadata = require('../utils/getMetadata.js');
 const userExists = require('../utils/userExists.js');
+const noop = require('lodash/noop');
+const get = require('lodash/get');
+const { USERS_ALIAS_FIELD } = require('../constants.js');
+
+function isPublic(username, audience) {
+  return metadata => {
+    if (get(metadata, [audience, USERS_ALIAS_FIELD]) === username) {
+      return;
+    }
+
+    throw new Errors.HttpStatusError(404, 'username was not found');
+  };
+}
 
 module.exports = function getMetadataAction(message) {
-  const { audience } = message;
+  const { audience, username } = message;
 
   return Promise
-    .bind(this, message.username)
+    .bind(this, username)
     .then(userExists)
-    .then(username => [username, audience])
-    .spread(getMetadata);
+    .then(realUsername => [realUsername, audience])
+    .spread(getMetadata)
+    .tap(message.public ? isPublic(username, audience) : noop);
 };

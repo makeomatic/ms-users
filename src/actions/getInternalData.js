611e3ee3c7e2dcd9e07bf41a3f0fa9d880b4f202
Vitaly Aminev <v@aminev.me>
2016-02-23 23:35:16 +0300

    feat(getInternalData): allow filtering returned fields
    Supply `fields` array with names of fields to return

--- src/actions/getInternalData.js
+++ src/actions/getInternalData.js
@@ -1,8 +1,14 @@
 const Promise = require('bluebird');
 const getInternalData = require('../utils/getInternalData.js');
+const pick = require('lodash/pick');
 
 module.exports = function internalData(message) {
+  const { fields } = message;
+
   return Promise
     .bind(this, message.username)
-    .then(getInternalData);
+    .then(getInternalData)
+    .then(data => {
+      return fields ? pick(data, fields) : data;
+    });
 };

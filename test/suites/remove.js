4940ed44566212a4d5c3d84307f4a262b7773f44
AVVS <v@aminev.me>
2016-05-17 16:14:48 -0700

    tests: fix usernames used to remove

--- test/suites/remove.js
+++ test/suites/remove.js
@@ -10,7 +10,7 @@ describe('#remove', function registerSuite() {
 
   // register 3 users
   beforeEach(globalRegisterUser('admin@me.com', { metadata: { roles: [USERS_ADMIN_ROLE] } }));
-  beforeEach(globalRegisterUser('normal-2@me.com'));
+  beforeEach(globalRegisterUser('normal-1@me.com'));
 
   it('must reject invalid registration params and return detailed error', function test() {
     return this.users
@@ -31,6 +31,17 @@ describe('#remove', function registerSuite() {
       .then(registered => {
         assert.equal(registered.name, 'HttpStatusError');
         assert.equal(registered.statusCode, 400);
+      });
+  });
+
+  it('must fail to remove non-existing user', function test() {
+    return this.users
+      .router({ username: 'normal-2@me.com' }, headers)
+      .reflect()
+      .then(inspectPromise(false))
+      .then(registered => {
+        assert.equal(registered.name, 'HttpStatusError');
+        assert.equal(registered.statusCode, 404);
       });
   });
 

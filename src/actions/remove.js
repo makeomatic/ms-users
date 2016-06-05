e04d2c718b6d3d2ee0f39525a78edd2d04c73151
Vadim Egorov <stainwoortsel@gmail.com>
2016-06-05 17:41:31 +0300

    feat: remove action

--- src/actions/remove.js
+++ src/actions/remove.js
@@ -1,25 +1,16 @@
 const Promise = require('bluebird');
 const Errors = require('common-errors');
 const key = require('../utils/key');
-const getInternalData = require('../utils/getInternalData');
-const getMetadata = require('../utils/getMetadata');
-const {
-  USERS_INDEX,
-  USERS_PUBLIC_INDEX,
-  USERS_ALIAS_TO_LOGIN,
-  USERS_DATA,
-  USERS_METADATA,
-  USERS_TOKENS,
-  USERS_ALIAS_FIELD,
-  USERS_ADMIN_ROLE,
-} = require('../constants');
+const { USERS_ADMIN_ROLE } = require('../constants');
+
+const Users = require('../db/adapter');
 
 module.exports = function removeUser({ username }) {
   const audience = this.config.jwt.defaultAudience;
 
   return Promise.props({
-    internal: getInternalData.call(this, username),
-    meta: getMetadata.call(this, username, audience),
+    internal: Users.getUser(username),
+    meta: Users.getMetadata(username, audience)
   })
   .then(({ internal, meta }) => {
     const isAdmin = (meta[audience].roles || []).indexOf(USERS_ADMIN_ROLE) >= 0;
@@ -27,24 +18,6 @@ module.exports = function removeUser({ username }) {
       throw new Errors.HttpStatusError(400, 'can\'t remove admin user from the system');
     }
 
-    const transaction = this.redis.multi();
-    const alias = internal[USERS_ALIAS_FIELD];
-    if (alias) {
-      transaction.hdel(USERS_ALIAS_TO_LOGIN, alias);
-    }
-
-    // clean indices
-    transaction.srem(USERS_PUBLIC_INDEX, username);
-    transaction.srem(USERS_INDEX, username);
-
-    // remove metadata & internal data
-    transaction.del(key(username, USERS_DATA));
-    transaction.del(key(username, USERS_METADATA, audience));
-
-    // remove auth tokens
-    transaction.del(key(username, USERS_TOKENS));
-
-    // complete it
-    return transaction.exec();
+    return Users.removeUser(username, internal);
   });
 };

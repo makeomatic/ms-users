20a92868498bf3624c139eadb4ff9d76d3d6d92a
AVVS <v@aminev.me>
2016-06-26 23:55:04 +0300

    docs: fix adding dir to commit

--- docker-release.sh
+++ docker-release.sh
@@ -11,6 +11,7 @@ if [ x"$BRANCH_NAME" == x"master" ] && [ x"$SEMAPHORE" == x"true" ]; then
   npm run doc
   git config user.email "semaphore@makeomatic.co"
   git config user.name "semaphore"
+  git add ./docs
   git commit -m "docs($SEMAPHORE_BUILD_NUMBER): updated remote public documentation"
   git push origin `git subtree split --prefix docs master`:gh-pages --force
 fi

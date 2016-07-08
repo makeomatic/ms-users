aaad1a40af45b9f1ba72b2d5fbb635375a79bdf1
Vadim Egorov <stainwoortsel@gmail.com>
2016-07-09 00:07:25 +0300

    fix: please linter, please...

--- src/users.js
+++ src/users.js
@@ -4,7 +4,7 @@ const Errors = require('common-errors');
 const merge = require('lodash/merge');
 const fsort = require('redis-filtered-sort');
 const { NotImplementedError } = Errors;
-const defaultOpts = require('./defaults.js');
+const _defaultOpts = require('./defaults.js');
 
 /**
  * @namespace Users
@@ -15,7 +15,7 @@ module.exports = class Users extends Mservice {
    * Configuration options for the service
    * @type {Object}
    */
-  static defaultOpts = defaultOpts;
+  static defaultOpts = _defaultOpts;
 
   /**
    * @namespace Users

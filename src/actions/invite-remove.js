f6b6370786ca3efcca1bd6eab8a55c5d533b9315
AVVS <v@makeomatic.ru>
2016-08-27 23:21:08 +0300

    feat: `users.invit-remove` endpoint

---
+++ src/actions/invite-remove.js
@@ -0,0 +1,24 @@
+const {
+  INVITATIONS_INDEX,
+  USERS_ACTION_INVITE,
+} = require('../constants.js');
+
+/**
+ * @api {amqp} <prefix>.invite-remove Generate User Invitation
+ * @apiVersion 1.0.0
+ * @apiName removeinvite
+ * @apiGroup Users
+ *
+ * @apiDescription Removes existing invitation by it's id. Make sure that only admin can access this route
+ *
+ * @apiParam (Payload) {Object} id - id of the invitation
+ *
+ */
+module.exports = function generateInvite(request) {
+  const { redis, tokenManager } = this;
+  const { id } = request.params;
+
+  return tokenManager
+    .remove({ id, action: USERS_ACTION_INVITE })
+    .tap(() => redis.srem(INVITATIONS_INDEX, id));
+};

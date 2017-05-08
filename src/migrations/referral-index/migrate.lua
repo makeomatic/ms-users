89a11045548a2de9c28ae8a6e86b9a13f5d7cc86
AVVS <v@makeomatic.ru>
2017-05-08 10:55:14 -0700

    fix(migration/referral-index): decode referral code from json

--- src/migrations/referral-index/migrate.lua
+++ src/migrations/referral-index/migrate.lua
@@ -9,7 +9,7 @@ local uids = redis.call('lrange', usersIdsList, 0, -1)
 for i, uid in ipairs(uids) do
   local metaKey = usersMeta:gsub('(uid)', uid, 1)
   local referral = redis.call('hget', metaKey, referralField)
-  local referralIndex = usersReferralIndex:gsub('(uid)', referral, 1)
+  local referralIndex = usersReferralIndex:gsub('(uid)', cjson.decode(referral), 1)
 
   redis.call('sadd', referralIndex, uid);
 end

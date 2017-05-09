a330e243c4f43b2330134294088109234d8bd707
Vitaly Aminev <AVVS@users.noreply.github.com>
2017-05-08 19:59:45 -0700

    feat(isReferral): adds isReferral action (#312)
    * feat(isReferral): adds isReferral action isReferral action allows one to determine if alias/username/id is a referral of a specific referral code in the system
    * fix(register): correctly add user to referral index on instant activation

---
+++ src/actions/isReferral.js
@@ -0,0 +1,42 @@
+const Promise = require('bluebird');
+const userExists = require('../utils/userExists');
+const { USERS_REFERRAL_INDEX } = require('../constants');
+
+/**
+ * Verifies
+ * @return {Boolean} [description]
+ */
+function checkIndex(username) {
+  return this.redis
+    .sismember(`${USERS_REFERRAL_INDEX}:${this.referralCode}`, username)
+    .then(yes => (yes === 1 ? username : false));
+}
+
+/**
+ * @api {amqp} <prefix>.isReferral Checks if username is a referral
+ * @apiVersion 1.0.0
+ * @apiName verifyReferral
+ * @apiGroup Users
+ *
+ * @apiDescription Verifies if <username> is a referral of <referralCode>. If username is a referral - returns true id,
+ *   otherwise false
+ *
+ * @apiParam (Payload) {String} username
+ * @apiParam (Payload) {String} referralCode
+ */
+function isReferral({ params }) {
+  const { username, referralCode } = params;
+  const { redis } = this;
+
+  return Promise
+    .bind(this, username)
+    .then(userExists)
+    .bind({
+      referralCode,
+      redis,
+    })
+    .then(checkIndex)
+    .catchReturn(false);
+}
+
+module.exports = isReferral;

3ba814cdccb39feeac3bee3d12d1864a12df1513
AVVS <v@makeomatic.ru>
2017-05-14 13:50:18 -0700

    Feat: node 7.10.0 & be able to init users with referrals
    This is mainly useful for testing

--- src/accounts/init-admin.js
+++ src/accounts/init-admin.js
@@ -1,7 +1,9 @@
 const Promise = require('bluebird');
 const defaults = require('lodash/defaults');
-const register = require('../actions/register.js');
-const { USERS_ADMIN_ROLE, CHALLENGE_TYPE_EMAIL } = require('../constants.js');
+const register = require('../actions/register');
+const uuid = require('uuid');
+const saveReferral = require('../actions/referral');
+const { USERS_ADMIN_ROLE, CHALLENGE_TYPE_EMAIL } = require('../constants');
 
 module.exports = function initAccounts() {
   const config = this.config;
@@ -12,25 +14,40 @@ module.exports = function initAccounts() {
     .delay(config.initAdminAccountsDelay)
     .return(accounts)
     // @todo use router dispatch, because a schema can contains default params
-    .map(account => register
-      .call(this, {
+    .map((account) => {
+      const userData = {
+        audience,
+        username: account.username,
+        password: account.password,
+        metadata: defaults(account.metadata || {}, {
+          firstName: account.firstName,
+          lastName: account.lastName,
+          roles: [USERS_ADMIN_ROLE],
+        }),
+        activate: true,
+        challengeType: CHALLENGE_TYPE_EMAIL,
+        skipPassword: false,
+      };
+
+      if (!account.referral) {
+        return { params: userData };
+      }
+
+      userData.referral = uuid.v4();
+
+      // this will be performed each time on startup, but shouldnt be a problem due to NX
+      // and lack of side-effect
+      return saveReferral.call(this, {
         params: {
-          audience,
-          username: account.username,
-          password: account.password,
-          metadata: defaults(account.metadata || {}, {
-            firstName: account.firstName,
-            lastName: account.lastName,
-            roles: [USERS_ADMIN_ROLE],
-          }),
-          activate: true,
-          challengeType: CHALLENGE_TYPE_EMAIL,
-          skipPassword: false,
+          id: userData.referral,
+          referral: account.referral,
         },
       })
-      .reflect()
-    )
+      .return(userData);
+    })
     .bind(this)
+    .then(register)
+    .reflect()
     .then(function reportStats(users) {
       const totalAccounts = users.length;
       const errors = [];

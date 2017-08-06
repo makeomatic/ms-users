502c5a48a4fb36daad39f0731a06d68028c5eaa6
AVVS <v@makeomatic.ru>
2017-08-05 17:22:35 -0700

    chore(tests): increase startup timeout

--- test/config.js
+++ test/config.js
@@ -84,8 +84,7 @@ function registerUser(username, opts = {}) {
       activate: !opts.inactive,
       skipChallenge: true,
       metadata: opts.metadata || undefined,
-    })
-    .then(() => {
+    }).then(() => {
       if (opts.locked) {
         return dispatch('users.ban', { username, ban: true });
       }

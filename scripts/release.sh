f02f04c513cede9e2815be493d9d3fc06b3ac79e
AVVS <v@makeomatic.ru>
2017-09-29 16:50:42 -0700

    patch: print status pre-doc

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,6 +8,7 @@ git config user.email "semaphore@makeomatic.co"
 git config user.name "semaphore"
 touch ./docs/.nojekyll
 git add -f ./docs
+git status
 git commit -m "chore(docs-$SEMAPHORE_BUILD_NUMBER): updated remote public documentation"
 git push origin `git subtree split --prefix docs master`:gh-pages --force
 

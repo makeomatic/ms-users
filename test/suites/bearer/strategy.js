19d587e3f179bb5fd5e6cfe0b2d70e59a7a3ed16
AVVS <v@makeomatic.ru>
2018-06-05 17:22:34 -0700

    feat: accept bearer token in strategy.bearer

--- test/suites/bearer/strategy.js
+++ test/suites/bearer/strategy.js
@@ -1,4 +1,5 @@
 const { inspectPromise } = require('@makeomatic/deploy');
+const Promise = require('bluebird');
 const assert = require('assert');
 const request = require('request-promise').defaults({
   uri: 'http://ms-users.local:3000/users/_/me',
@@ -69,43 +70,71 @@ describe('/_/me', function verifySuite() {
 
   describe('valid token', function suite() {
     const jwt = require('../../../src/utils/jwt');
+    let bearerAuthHeaders;
+    let jwtAuthHeaders;
 
-    beforeEach(function pretest() {
-      return this.dispatch('users.register', {
-        username: 'v@makeomatic.ru',
-        password: '123',
-        audience: 'test',
-        metadata: {
-          fine: true,
-        },
-      });
-    });
+    const verifyBody = (body) => {
+      assert.ok(body.id);
+      assert.equal(body.metadata['*.localhost'].username, 'v@makeomatic.ru');
+      assert.deepEqual(body.metadata.test, { fine: true });
+    };
 
     beforeEach(async function pretest() {
-      const data = await jwt.login.call(this.users, 'v@makeomatic.ru', 'test');
-      this.token = data.jwt;
+      await this.users.dispatch('register', {
+        params: {
+          username: 'v@makeomatic.ru',
+          password: '123',
+          audience: 'test',
+          metadata: {
+            fine: true,
+          },
+        },
+      });
+
+      const [bearer, jwtTokenData] = await Promise.all([
+        this.users.dispatch('token.create', {
+          params: {
+            username: 'v@makeomatic.ru',
+            name: 'sample',
+          },
+        }),
+        jwt.login.call(this.users, 'v@makeomatic.ru', 'test'),
+      ]);
+
+      bearerAuthHeaders = { authorization: `Bearer ${bearer}` };
+      jwtAuthHeaders = { authorization: `JWT ${jwtTokenData.jwt}` };
     });
 
-    it('must return user object and required audiences information on a valid JWT token', function test() {
-      return request
-        .get({ headers: { authorization: `JWT ${this.token}` }, qs: { audience: 'test' } })
-        .then((body) => {
-          assert.ok(body.id);
-          assert.equal(body.metadata['*.localhost'].username, 'v@makeomatic.ru');
-          assert.deepEqual(body.metadata.test, { fine: true });
-        });
+    it('must return user object and required audiences information on a valid JWT token', async () => {
+      const body = await request
+        .get({ headers: jwtAuthHeaders, qs: { audience: 'test' } });
+
+      verifyBody(body);
     });
 
-    it('must return user object and required audiences information on a valid JWT token', function test() {
-      return this.users.amqp
-        .publishAndWait('users._.me', { audience: 'test' }, {
-          headers: { authorization: `JWT ${this.token}` },
-        })
-        .then((body) => {
-          assert.ok(body.id);
-          assert.equal(body.metadata['*.localhost'].username, 'v@makeomatic.ru');
-          assert.deepEqual(body.metadata.test, { fine: true });
+    it('must return user object and required audiences information on a valid JWT token', async function test() {
+      const body = await this.users
+        .amqp.publishAndWait('users._.me', { audience: 'test' }, {
+          headers: jwtAuthHeaders,
         });
+
+      verifyBody(body);
+    });
+
+    it('must return user object and required audiences information on a valid Bearer token', async () => {
+      const body = await request
+        .get({ headers: bearerAuthHeaders, qs: { audience: 'test' } });
+
+      verifyBody(body);
+    });
+
+    it('must return user object and required audiences information on a valid Bearer token', async function test() {
+      const body = await this.users
+        .amqp.publishAndWait('users._.me', { audience: 'test' }, {
+          headers: bearerAuthHeaders,
+        });
+
+      verifyBody(body);
     });
   });
 });

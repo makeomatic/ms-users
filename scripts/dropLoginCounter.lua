bc1c13644764a01dc987e1b20f4d74a8969f3146
Vladislav Lebedev <tonknaf@gmail.com>
2019-06-07 14:09:57 +0300

    fix: errs in lua drop login counter script

--- scripts/dropLoginCounter.lua
+++ scripts/dropLoginCounter.lua
@@ -2,9 +2,13 @@ local remote = KEYS[1]
 local globalRemote = KEYS[2]
 
 -- current attempts
-local attempts = redis.call("GET", remote) or 0
+local attempts = redis.call("GET", remote) or 0 
 
 -- delete remote
 -- reduce amount of failed sign ins for that ip address
 redis.call("DEL", remote)
-redis.call("INCRBY", globalRemote, -attempts)
+
+-- INCRBY with -0 will get "ERR value is not an integer or out of range"
+if tonumber(attempts) > 0 then -- lua may throw error comparing number to string
+  redis.call("INCRBY", globalRemote, -attempts)
+end

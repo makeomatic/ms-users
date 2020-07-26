07e484ef2e658d58acb9ee85b521d9033871c0ec
Eugene Poyarkov <eug.poyarkov@gmail.com>
2020-07-26 16:12:44 +0100

    chore: modify test case

--- test/suites/bin/password.js
+++ test/suites/bin/password.js
@@ -6,25 +6,23 @@ describe('binary: password', function suite() {
   const binaryPath = path.resolve(__dirname, '../../../bin/password.js');
   const username = 'test@test.me';
   const newPassword = 'trickynewpassword';
+  const checkAuth = globalAuthUser(username, newPassword);
 
   before(global.startService);
   before(() => global.globalRegisterUser(username, {
     inactive: false,
   }));
+  // eslint-disable-next-line no-undef
+  before(globalAuthUser(username));
   after(global.clearRedis);
 
   it('allows updating password from the command-line', async () => {
-    // eslint-disable-next-line no-undef
-    await globalAuthUser(username);
-    assert(this.userId);
-
     await exec(`${binaryPath} ${this.userId} ${newPassword}`);
-
     // Verify trying to log in with the new password
     // unset jwt token just in case
     this.jwt = null;
     // eslint-disable-next-line no-undef
-    await globalAuthUser(username, newPassword);
+    await checkAuth();
     assert(this.jwt);
   });
 });

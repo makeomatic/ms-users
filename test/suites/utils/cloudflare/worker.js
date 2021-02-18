9652c659676749379c6ea478f991c2035746a5cf
pajgo <pavel.rogovoi@gmail.com>
2021-02-18 20:06:27 +0600

    test: extra worker check

--- test/suites/utils/cloudflare/worker.js
+++ test/suites/utils/cloudflare/worker.js
@@ -1,6 +1,8 @@
 const { assert } = require('chai');
 const { once } = require('events');
+const sinon = require('sinon');
 
+const { CloudflareWorker } = require('../../../../src/utils/cloudflare/worker');
 const Users = require('../../../../src');
 
 const { nockCfApi, restoreCfApi } = require('../../../helpers/cloudflare/api-stub');
@@ -32,12 +34,17 @@ describe('#cloudflare access-list worker', () => {
         },
       },
     });
+    const startSpy = sinon.spy(CloudflareWorker.prototype, 'start');
 
     await this.users.connect();
     await this.users.cfAccessList.cfApi.createList('test_worker_test_list');
 
     const { cfWorker } = this.users;
     assert(cfWorker.next instanceof Timeout);
+
     await once(cfWorker, 'done');
+    await once(cfWorker, 'done');
+
+    assert(startSpy.callCount === 2, 'should be called twice');
   });
 });

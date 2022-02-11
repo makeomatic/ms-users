e9ea07ffac7b539fbfb01f1c5a9b8cde184d08d0
AVVS <v@makeomatic.ca>
2022-02-10 19:38:51 -0800

    chore: docs issues

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -4,7 +4,7 @@ set -ex
 
 # prepare docs
 current_branch=`git rev-parse --abbrev-ref HEAD`
-branch_name="docs/$SEMAPHORE_BUILD_NUMBER"
+branch_name="docs/$SEMAPHORE_JOB_ID"
 git checkout -b $branch_name
 cd ./schemas
 apidoc -i ../src/actions -v --debug -o ../docs
@@ -14,6 +14,6 @@ git config user.name "semaphore"
 touch ./docs/.nojekyll
 git add -f ./docs
 git status
-git commit -m "chore(docs-$SEMAPHORE_BUILD_NUMBER): updated remote public documentation"
+git commit -m "chore(docs-$SEMAPHORE_JOB_ID): updated remote public documentation"
 git push origin `git subtree split --prefix docs $branch_name`:gh-pages --force
 git checkout $current_branch

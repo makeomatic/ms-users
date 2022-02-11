a0f72600791595fa2757c44c420a2ac2d2321218
AVVS <v@makeomatic.ca>
2022-02-10 20:55:32 -0800

    chore: docs gen script fixes

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -6,14 +6,14 @@ set -ex
 current_branch=`git rev-parse --abbrev-ref HEAD`
 branch_name="docs/$SEMAPHORE_JOB_ID"
 git checkout -b $branch_name
-cd ./schemas
-apidoc -i ../src/actions -v --debug -o ../docs
-cd ..
+pnpm exec -- apidoc -i ./src/actions -v --debug -o ./docs
 git config user.email "semaphore@makeomatic.co"
 git config user.name "semaphore"
 touch ./docs/.nojekyll
 git add -f ./docs
 git status
 git commit -m "chore(docs-$SEMAPHORE_JOB_ID): updated remote public documentation"
-git push origin `git subtree split --prefix docs $branch_name`:gh-pages --force
+url=`node -e "console.log(require('./package.json').repository.url)"`
+origin=`echo $url | awk -F '//' '{print $1"//"ENVIRON["GITHUB_TOKEN"]"@"$2 }'`
+git push $origin `git subtree split --prefix docs $branch_name`:gh-pages --force
 git checkout $current_branch

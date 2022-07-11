a68c2976440d1a7978ca48766cfc38ac02280995
pajgo <pavel.rogovoi@gmail.com>
2022-07-11 16:29:23 +0600

    fix: export credential store

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,9 @@
 const { ConsulWatcher } = require('./utils/consul-watcher');
+const { CredentialsStore } = require('./utils/credentials-store');
 
 module.exports = {
   ConsulWatcher,
+  CredentialsStore,
   auth: {
     strategy: require('./auth/strategies'),
     statelessJWT: require('./utils/stateless-jwt'),

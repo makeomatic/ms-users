3989e51b05f54aee0924e7e90d7dc459afcf3c60
pajgo <pavel.rogovoi@gmail.com>
2022-07-14 16:03:33 +0600

    chore: cleanup

--- src/configs/auth.js
+++
@@ -1,5 +0,0 @@
-module.exports.signedRequest = {
-  // see https://www.npmjs.com/package/http-signature headers
-  headers: ['digest', '(request-target)', '(algorithm)', '(keyid)'],
-  clockSkew: 600, // seconds to invalidate request if 'x-date' or 'date' set
-};

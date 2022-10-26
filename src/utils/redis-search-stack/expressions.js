bb41b9c6eeb82f65b748efe86d638d2416b2e534
sky3d <sky3ddd@gmail.com>
2022-10-26 17:36:55 +0300

    fix: update expressions module export

--- src/utils/redis-search-stack/expressions.js
+++ src/utils/redis-search-stack/expressions.js
@@ -1,42 +1,36 @@
 /**
  * Search Query Syntax -  https://redis.io/docs/stack/search/reference/query_syntax/
  */
-export const ANY_WHILDCARD = '*';
-export const FIELD = '@';
-export const PARAM_REFERENCE = '$';
 
-export const VALUE_SEPARATOR = ':';
-export const PIPE_SEPARATOR = '|';
+const ANY_WHILDCARD = '*';
+const FIELD = '@';
+const PARAM_REFERENCE = '$';
+
+const VALUE_SEPARATOR = ':';
+const PIPE_SEPARATOR = '|';
 
 // const DIALECT_USE_PARAMS = '2';
 
-export const NEGATIVE = '-';
-export const POSITIVE = '+';
-export const INFINITY = 'inf';
-export const NEGATIVE_RANGE = `${NEGATIVE}${INFINITY}`;
-export const POSITIVE_RANGE = `${POSITIVE}${INFINITY}`;
+const NEGATIVE = '-';
+const POSITIVE = '+';
+const INFINITY = 'inf';
+const NEGATIVE_RANGE = `${NEGATIVE}${INFINITY}`;
+const POSITIVE_RANGE = `${POSITIVE}${INFINITY}`;
 
-export const namedField = (propName) => `${FIELD}${propName}`;
-export const paramRef = (name) => `${PARAM_REFERENCE}${name}`;
+module.exports = exports = {
+  namedField: (propName) => `${FIELD}${propName}`,
+  paramRef: (name) => `${PARAM_REFERENCE}${name}`,
 
-// Values
-export const selection = (value) => value;
+  // Values
+  selection: (value) => value,
+  union: (values = []) => `(${values.join(PIPE_SEPARATOR)})`,
+  numericRange: (min = NEGATIVE_RANGE, max = POSITIVE_RANGE) => `[${min} ${max}]`,
+  negative: (expr) => `${NEGATIVE}${expr}`,
+  tag: (item) => `{${item}}`,
+  tags: (items = []) => `{${items.join(PIPE_SEPARATOR)}}`,
+  // TODO check usage
+  matchAny: (value) => `(${value}${ANY_WHILDCARD})`,
 
-export const union = (values = []) => `(${values.join(PIPE_SEPARATOR)})`;
-
-export const numericRange = (min = NEGATIVE_RANGE, max = POSITIVE_RANGE) => `[${min} ${max}]`;
-
-export const negative = (expr) => `${NEGATIVE}${expr}`;
-
-export const tag = (item) => `{${item}}`;
-
-export const tags = (items = []) => `{${items.join(PIPE_SEPARATOR)}}`;
-
-// export const clause = (attributes, values) => {
-//   // TODO (foo bar) => { $weight: 2.0; $slop: 1; $inorder: false; }
-// };
-
-// TODO check usage
-export const matchAny = (value) => `(${value}${ANY_WHILDCARD})`;
-
-export const expression = (name, value) => `${name}${VALUE_SEPARATOR}${value}`;
+  // clause: = (foo bar) => { $weight: 2.0, $slop: 1, $inorder: false, }
+  expression: (name, value) => `${name}${VALUE_SEPARATOR}${value}`,
+};

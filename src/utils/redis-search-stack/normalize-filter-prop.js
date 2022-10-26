01ee800cf908bc3fac6e27f0a7c3d2cf8276ce36
sky3d <sky3ddd@gmail.com>
2022-10-26 17:36:55 +0300

    fix: update expressions module export

--- src/utils/redis-search-stack/normalize-filter-prop.js
+++ src/utils/redis-search-stack/normalize-filter-prop.js
@@ -1,12 +1,11 @@
 const { USERS_ID_FIELD } = require('../../constants');
-const { PIPE_SEPARATOR } = require('./expressions');
 
 const normalizeFilterProp = (propName, actionTypeOrValue) => {
   let field = propName;
   if (field === '#') { // id
     field = USERS_ID_FIELD;
   } else if (field === '#multi') {
-    field = actionTypeOrValue.fields.join(PIPE_SEPARATOR);
+    field = actionTypeOrValue.fields.join('|');
   }
   // else if (field === 'alias') {
   //   field = 'alias_tag';

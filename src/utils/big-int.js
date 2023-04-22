c3d1578e97960980edd6f5f3e0456e603c73b0fc
sky3d <sky3ddd@gmail.com>
2023-04-22 14:43:56 +0300

    fix: improve bigInt comparision

--- src/utils/big-int.js
+++ src/utils/big-int.js
@@ -1,11 +1,13 @@
-function bigIntComparer(a, b) {
-  const sign = BigInt(a) - BigInt(b);
+const ZERO = BigInt(0);
 
-  if (sign === 0) {
+function bigIntComparer(a, b) {
+  const diff = BigInt(a) - BigInt(b);
+
+  if (diff === ZERO) {
     return 0;
   }
 
-  return sign < 0 ? -1 : 1;
+  return diff < ZERO ? -1 : 1;
 }
 
 module.exports = {

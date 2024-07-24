cb926221a7845e6538354914938588425476967f
OT <54023298+o-cappasity@users.noreply.github.com>
2024-07-24 20:55:40 +0300

    fix: email to contacts migration (#609)
    ---------
    
    Co-authored-by: AVVS <v@makeomatic.ca>
    Co-authored-by: Vitaly Aminev <AVVS@users.noreply.github.com>

--- .mdeprc.js
+++ .mdeprc.js
@@ -14,7 +14,7 @@ if (os.platform() !== 'darwin') {
   } catch (e) { }
 }
 
-exports.node = "20";
+exports.node = "20.15.1";
 exports.in_one = true;
 exports.auto_compose = true;
 exports.with_local_compose = true;

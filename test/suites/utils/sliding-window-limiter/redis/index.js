57191fcc958b98a07621a4e1bdc0ca72a64a582b
Oleg Tokar <o.tokar@cappasity.com>
2024-10-27 10:46:44 +0200

    fix: rm fake timers

--- test/suites/utils/sliding-window-limiter/redis/index.js
+++ test/suites/utils/sliding-window-limiter/redis/index.js
@@ -262,7 +262,7 @@ describe('#sliding-window-limiter', function SlidingWindowLimiterTestSuite() {
       };
 
       beforeEach(function replaceClock() {
-        clock = sinon.useFakeTimers({ now: 200000, toFake: ['setTimeout', 'setInterval'] });
+        clock = sinon.useFakeTimers(200000);
       });
 
       afterEach(function restoreClock() {
@@ -332,7 +332,7 @@ describe('#sliding-window-limiter', function SlidingWindowLimiterTestSuite() {
       };
 
       beforeEach(function replaceClock() {
-        clock = sinon.useFakeTimers({ now: 200000, toFake: ['setTimeout', 'setInterval'] });
+        clock = sinon.useFakeTimers(200000);
       });
 
       afterEach(function restoreClock() {

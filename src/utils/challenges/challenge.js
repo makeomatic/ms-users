d19c640d7e9245d4014a8371a7c72c69a33b5764
Sasha S <sorokin.sh@gmail.com>
2024-11-15 13:17:07 +0300

    fix: phone config for challenge action

--- src/utils/challenges/challenge.js
+++ src/utils/challenges/challenge.js
@@ -36,7 +36,8 @@ async function generateChallenge(type, opts, ctx = {}, wait = false) {
     if (error.message === '429') {
       const { mailto: email } = this.config.validation;
       const duration = moment().add(opts.throttle, 'seconds').toNow(true);
-      const msg = `We've already sent you an email, if it doesn't come - please try again in ${duration} or send us an email`;
+      const method = type === CHALLENGE_TYPE_EMAIL ? CHALLENGE_TYPE_EMAIL : 'code';
+      const msg = `We've already sent you an ${method}, if it doesn't come - please try again in ${duration} or send us an email`;
       const reason = { ...error.reason, duration: opts.throttle, email };
 
       throw DetailedHttpStatusError(429, msg, reason);
